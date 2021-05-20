54b569157a6bca5a424ae818b9d872a0a7fe4ae6
Roger Aiudi <aiudirog@gmail.com>
2021-05-19 23:46:39 -0400

    Add split() to divide iterable on condition

--- aiuti/itertools.py
+++ aiuti/itertools.py
@@ -10,10 +10,15 @@ more_itertools.
 
 __all__ = [
     'exhaust',
+    'split',
 ]
 
+import operator as op
 from collections import deque
-from typing import Iterable, Any
+from itertools import tee, compress, cycle
+from typing import Tuple, Iterable, Iterator, Callable, Union, Any
+
+from .typing import T
 
 
 def exhaust(iterable: Iterable[Any]):
@@ -30,3 +35,43 @@ def exhaust(iterable: Iterable[Any]):
     """
     # deque is ~10% faster than writing a loop
     deque(iterable, maxlen=0)
+
+
+def split(
+    iterable: Iterable[T],
+    condition: Union[Iterable[bool], Callable[[T], bool]],
+) -> Tuple[Iterator[T], Iterator[T]]:
+    """
+    Similar to :func:`itertools.compress` except that it returns two
+    iterators: one where with the values where the condition is True
+    and one where with the values where the condition is False.
+
+    >>> evens, odds = split(range(5), cycle((True, False)))
+    >>> list(evens)
+    [0, 2, 4]
+    >>> list(odds)
+    [1, 3]
+
+    The condition can also be a boolean callable:
+
+    >>> evens, odds = split(range(5), lambda x: x % 2 == 0)
+    >>> list(evens)
+    [0, 2, 4]
+    >>> list(odds)
+    [1, 3]
+
+    :param iterable:
+        Iterable to split into two sub-iterators.
+        If given as iterator, it must not be advanced directly later!
+    :param condition:
+        Either a boolean callable to map to each element or a boolean
+        iterable indicating which values belong in which set.
+        If given as iterator, it must not be advanced directly later!
+    :return: Tuple of two iterators: (where_true, where_false)
+    """
+    if callable(condition):
+        iterable, ci = tee(iterable)
+        condition = map(condition, ci)
+    i1, i2 = tee(iterable)
+    c1, c2 = tee(condition)
+    return compress(i1, c1), compress(i2, map(op.not_, c2))
